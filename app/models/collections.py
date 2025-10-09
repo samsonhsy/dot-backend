78a75bdf0baef8d732df2957fef417ce136b0feb
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-09 17:33:44 +0800

    [feat](db) setup table collections model

---
+++ app/models/collections.py
@@ -0,0 +1,17 @@
+# app/models/collections.py
+from sqlite3.dbapi2 import Timestamp
+from sqlalchemy import Boolean, Text, Column, Integer, String, TIMESTAMPTZ, func
+from app.db.database import Base
+
+class Collections(Base):
+    __tablename__ = "collections"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(Text)
+    owner_id = Column(Integer, nullable=False)
+    is_private = Column(Boolean, nullable=False)
+    created_at = Column(TIMESTAMPTZ, server_default=func.now())
+    updated_at = Column(TIMESTAMPTZ, server_default=func.now())
+
+                
