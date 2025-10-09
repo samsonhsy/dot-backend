0d3e9e3d8bbd3d297fd1962ec4d3c1dfa0e70c96
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-09 17:22:41 +0800

    [feat](db) setup table dotfiles model

---
+++ app/models/dotfiles.py
@@ -0,0 +1,13 @@
+# app/models/dotfiles.py
+from sqlite3.dbapi2 import Timestamp
+from sqlalchemy import Text, Column, Integer, String, TIMESTAMPTZ, func
+from app.db.database import Base
+
+class Dotfiles(Base):
+    __tablename__ = "dotfiles"
+    
+    id = Column(Integer, primary_key=True)
+    collection_id = Column(Integer, nullable=False)
+    path = Column(String(255), nullable=False) # Can change string length later
+    content = Column(Text, nullable=False)
+                
