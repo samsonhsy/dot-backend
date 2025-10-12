e6b61c2528cf099fba092d72cfbf06c598fcb0fe
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-12 10:19:58 +0800

    [feat](db) add foreign keys to collections and dotfiles table models

--- app/models/collections.py
+++ app/models/collections.py
@@ -1,6 +1,6 @@
 # app/models/collections.py
 from sqlite3.dbapi2 import Timestamp
-from sqlalchemy import Boolean, Text, Column, Integer, String, TIMESTAMPTZ, func
+from sqlalchemy import Boolean, ForeignKey, Text, Column, Integer, String, TIMESTAMPTZ, func
 from app.db.database import Base
 
 class Collections(Base):
@@ -9,7 +9,7 @@ class Collections(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     description = Column(Text)
-    owner_id = Column(Integer, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     is_private = Column(Boolean, nullable=False)
     created_at = Column(TIMESTAMPTZ, server_default=func.now())
     updated_at = Column(TIMESTAMPTZ, server_default=func.now())
--- app/models/dotfiles.py
+++ app/models/dotfiles.py
@@ -1,13 +1,13 @@
 # app/models/dotfiles.py
 from sqlite3.dbapi2 import Timestamp
-from sqlalchemy import Text, Column, Integer, String
+from sqlalchemy import ForeignKey, Text, Column, Integer, String
 from app.db.database import Base
 
 class Dotfiles(Base):
     __tablename__ = "dotfiles"
     
     id = Column(Integer, primary_key=True)
-    collection_id = Column(Integer, nullable=False)
+    collection_id = Column(Integer, ForeignKey("collections.id"), nullable=False)
     path = Column(String(255), nullable=False) # Can change string length later
     content = Column(Text, nullable=False)
                 
