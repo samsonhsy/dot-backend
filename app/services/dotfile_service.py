6bbcbe9df22049adb67f357fcad2471ba2dd7b53
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-14 23:36:06 +0800

    [feat](dotfile) implement dotfile bulk create service function

---
+++ app/services/dotfile_service.py
@@ -0,0 +1,24 @@
+# app/services/dotfile_service.py
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from app.models.dotfiles import Dotfile
+from app.schemas.dotfiles import DotfileCreate
+
+async def create_dotfiles_in_collection(db: AsyncSession, dotfiles : list[DotfileCreate], collection_id : int) -> list[Dotfile]:
+    db_dotfiles = [Dotfile(path=dotfile.path, content=dotfile.content, collection_id=collection_id) for dotfile in dotfiles]
+        
+    db.add_all(db_dotfiles)
+
+    await db.commit()
+    
+    # refresh all dotfile row entries created
+    refesh_statement = (
+    select(Dotfile)
+    .where(Dotfile.path.in_([db_dotfile.path for db_dotfile in db_dotfiles]))
+    .execution_options(populate_existing=True)
+    )
+    
+    await db.execute(refesh_statement).scalars().all()
+
+    return db_dotfiles
