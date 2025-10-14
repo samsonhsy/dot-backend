257f6580094310aee4b70af078150447c6f874e1
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-14 23:11:05 +0800

    [feat](collection) implement collection create and retrieve function

---
+++ app/services/collection_service.py
@@ -0,0 +1,21 @@
+# app/services/collection_service.py
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from app.models.collections import Collections
+from schemas.collections import CollectionOutput
+
+async def get_collections_by_user(db: AsyncSession, user_id: int) -> list[CollectionOutput]:
+    result = await db.execute(select(Collections).filter(Collections.owner_id == user_id))
+    return result.scalars().all()
+
+async def create_collection(db: AsyncSession, collection: CollectionCreate, user_id: int) -> CollectionOutput:
+    db_collection = Collections(name=collection.name, description=collection.description, owner_id=user_id)
+    db.add(db_collection)
+
+    await db.commit()
+    await db.refresh(db_collection)
+
+    # TO DO: Add create dotfiles related to collection function
+
+    return db_collection
