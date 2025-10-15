b95bfaec890883ec154d4571addf226270175e90
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-15 20:54:37 +0800

    [feat](collection) add basic collection access verification function

--- app/services/collection_service.py
+++ app/services/collection_service.py
@@ -8,6 +8,17 @@ from app.schemas.collections import CollectionCreate
 
 from app.services.dotfile_service import create_dotfiles_in_collection
 
+async def get_access_to_collection_for_user(db: AsyncSession, collection_id: int, user_id: int) -> bool:
+    result = await db.execute(select(Collection).filter(Collection.id == collection_id))
+    collection = result.scalars().first()
+
+    if not collection.is_private:
+        return True
+
+    user_is_owner = (collection.owner_id == user_id)
+
+    return user_is_owner
+
 async def get_collections_by_user_id(db: AsyncSession, user_id: int) -> Optional[list[Collection]]:
     result = await db.execute(select(Collection).filter(Collection.owner_id == user_id))
     return result.scalars().all()
