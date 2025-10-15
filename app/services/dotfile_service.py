8f76ef87b46d7e4ed20cb065990fbc6a4526203c
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-15 21:32:58 +0800

    [feat](dotfile) implement dotfile retrieve function

--- app/services/dotfile_service.py
+++ app/services/dotfile_service.py
@@ -5,6 +5,10 @@ from sqlalchemy.future import select
 from app.models.dotfiles import Dotfile
 from app.schemas.dotfiles import DotfileCreate
 
+async def get_dotfiles_by_collection_id(db: AsyncSession, collection_id: int) -> list[Dotfile]:
+    result = await db.execute(select(Dotfile).filter(Dotfile.collection_id == collection_id))
+    return result.scalars.all()
+
 async def create_dotfiles_in_collection(db: AsyncSession, dotfiles : list[DotfileCreate], collection_id : int) -> list[Dotfile]:
     db_dotfiles = [Dotfile(path=dotfile.path, content=dotfile.content, collection_id=collection_id) for dotfile in dotfiles]
         
