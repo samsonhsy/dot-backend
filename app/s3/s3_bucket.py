aee866d8540b084a748fda9d9d1540cf3846153a
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-29 14:49:00 +0800

    [feat](s3) refactor s3_bucket.py to use aioboto3 instead of boto3

--- app/s3/s3_bucket.py
+++ app/s3/s3_bucket.py
@@ -1,9 +1,11 @@
-import boto3
+# app/s3/s3_bucket.py
+import aioboto3 # needed for aibotocore.session
+import aiobotocore
 from app.core.settings import settings
 
-session = boto3.session.Session()
+session = aiobotocore.session.get_session()
 
-s3_client = session.client(
+s3_client = session.create_client(
     service_name='s3',
     aws_access_key_id=settings.FILE_STORAGE_ACCESS_KEY_ID,
     aws_secret_access_key=settings.FILE_STORAGE_SECRET_ACCESS_KEY,
@@ -11,5 +13,8 @@ s3_client = session.client(
     region_name=settings.FILE_STORAGE_REGION
 )
 
+BUCKET_NAME = 'dot-s3'
+
 async def get_s3_client():
-    return s3_client
+   async with s3_client as s3:        
+        return s3
