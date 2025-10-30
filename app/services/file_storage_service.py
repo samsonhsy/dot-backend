0eca0196f85add97600d971d303dcc826317f2f1
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-10-30 17:59:19 +0800

    [feat](s3) add exception handling to file storage service

--- app/services/file_storage_service.py
+++ app/services/file_storage_service.py
@@ -1,14 +1,26 @@
 # app/services/file_storage_service.py
 import aioboto3
 from aiobotocore.session import ClientCreatorContext as S3Client
-from fastapi import UploadFile
+from fastapi import UploadFile, HTTPException
 from app.s3.s3_bucket import BUCKET_NAME
 
 async def upload_file_to_storage(s3 : S3Client, file : UploadFile):
-    await s3.put_object(Body=file, Bucket=BUCKET_NAME, Key=file.filename)  
+    if not file:
+        raise HTTPException(status_code=400, detail="Uploaded file does not exist")
+
+    result = await s3.put_object(Body=file, Bucket=BUCKET_NAME, Key=file.filename)
+
+    return result  
 
 async def retrieve_file_from_storage_by_filename(s3 : S3Client, filename : str):
-    await s3.get_object(Bucket=BUCKET_NAME, Key=filename) 
+    result = await s3.get_object(Bucket=BUCKET_NAME, Key=filename)
+
+    if not result:
+        raise HTTPException(status_code=404, detail=f"File {filename} not found")
+
+    return result 
 
 async def delete_file_from_storage_by_filename(s3 : S3Client, filename : str):
-    await s3.delete_object(Bucket=BUCKET_NAME, Key=filename)
+    result = await s3.delete_object(Bucket=BUCKET_NAME, Key=filename)
+    
+    return result
