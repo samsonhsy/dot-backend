e4aeb7b7c3865fe81b5a056da41d8e9d3f41a46e
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-11-03 23:07:17 +0800

    [fix](s3) move create_client() code into get_s3_client()

--- app/s3/s3_bucket.py
+++ app/s3/s3_bucket.py
@@ -5,16 +5,16 @@ from app.core.settings import settings
 
 session = aiobotocore.session.get_session()
 
-s3_client = session.create_client(
-    service_name='s3',
-    aws_access_key_id=settings.FILE_STORAGE_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.FILE_STORAGE_SECRET_ACCESS_KEY,
-    endpoint_url=settings.FILE_STORAGE_URL,
-    region_name=settings.FILE_STORAGE_REGION
-)
-
 BUCKET_NAME = 'dot-s3'
 
 async def get_s3_client():
-   async with s3_client as s3:        
+    s3_client = await session.create_client(
+        service_name='s3',
+        aws_access_key_id=settings.FILE_STORAGE_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.FILE_STORAGE_SECRET_ACCESS_KEY,
+        endpoint_url=settings.FILE_STORAGE_URL,
+        region_name=settings.FILE_STORAGE_REGION
+    )
+    
+    async with s3_client as s3:        
         return s3
