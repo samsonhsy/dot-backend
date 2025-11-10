673a4e8b4f3ed44f9c73a2f5c98b43e3f9050d71
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-11-11 02:29:46 +0800

    [refactor](test) rename PREFIX to USERS_PREFIX and add AUTH_PREFIX for tidiness

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,9 @@
 # tests/test_users.py
-PREFIX = "/users"
+USERS_PREFIX = "/users"
+AUTH_PREFIX = "/auth"
 
 def test_valid_register(mock_client, user_create_payload):
-    response = mock_client.post(PREFIX + "/register", json=user_create_payload)
+    response = mock_client.post(USERS_PREFIX + "/register", json=user_create_payload)
     
     status_code = response.status_code    
     assert status_code == 201
@@ -14,7 +15,7 @@ def test_valid_register(mock_client, user_create_payload):
     assert response_json["id"] == 1
 
 def test_duplicate_username_register(mock_client, user_create_payload):
-    original_register_response = mock_client.post(PREFIX + "/register", json=user_create_payload)
+    original_register_response = mock_client.post(USERS_PREFIX + "/register", json=user_create_payload)
 
     original_register_status_code = original_register_response.status_code
     assert original_register_status_code == 201
@@ -22,7 +23,7 @@ def test_duplicate_username_register(mock_client, user_create_payload):
     duplicate_username_create_payload = user_create_payload.copy()
     duplicate_username_create_payload["email"] = "new_mock_email@email.com"
 
-    duplicate_username_register_response = mock_client.post(PREFIX + "/register", json=duplicate_username_create_payload)
+    duplicate_username_register_response = mock_client.post(USERS_PREFIX + "/register", json=duplicate_username_create_payload)
     duplicate_username_register_status_code = duplicate_username_register_response.status_code
 
     assert duplicate_username_register_status_code == 400
@@ -31,7 +32,7 @@ def test_duplicate_username_register(mock_client, user_create_payload):
     assert duplicate_username_register_response_json["detail"] == "Username already exists"
 
 def test_duplicate_email_register(mock_client, user_create_payload):
-    original_register_response = mock_client.post(PREFIX + "/register", json=user_create_payload)
+    original_register_response = mock_client.post(USERS_PREFIX + "/register", json=user_create_payload)
 
     original_register_status_code = original_register_response.status_code
     assert original_register_status_code == 201
@@ -39,7 +40,7 @@ def test_duplicate_email_register(mock_client, user_create_payload):
     duplicate_email_create_payload = user_create_payload.copy()
     duplicate_email_create_payload["username"] = "new_mock_username"
 
-    duplicate_email_register_response = mock_client.post(PREFIX + "/register", json=duplicate_email_create_payload)
+    duplicate_email_register_response = mock_client.post(USERS_PREFIX + "/register", json=duplicate_email_create_payload)
     duplicate_email_register_status_code = duplicate_email_register_response.status_code
 
     assert duplicate_email_register_status_code == 400
@@ -51,14 +52,14 @@ def test_invalid_email_register(mock_client, user_create_payload):
     incorrect_user_create_payload = user_create_payload.copy()
     incorrect_user_create_payload["email"] = "invalid_mock_email"
     
-    invalid_email_register_response = mock_client.post(PREFIX + "/register", json=incorrect_user_create_payload)
+    invalid_email_register_response = mock_client.post(USERS_PREFIX + "/register", json=incorrect_user_create_payload)
 
     invalid_email_register_status_code = invalid_email_register_response.status_code
     assert invalid_email_register_status_code == 422
 
 def test_user_list(mock_client, user_create_payload):
     # get list of users when there are no registered users
-    empty_get_user_list_response = mock_client.get(PREFIX + "/")
+    empty_get_user_list_response = mock_client.get(USERS_PREFIX + "/")
     
     empty_get_user_list_status_code = empty_get_user_list_response.status_code
     assert empty_get_user_list_status_code == 200
@@ -67,13 +68,13 @@ def test_user_list(mock_client, user_create_payload):
     assert len(empty_get_user_list_response_json) == 0
     
     # create a first user
-    first_user_create_response = mock_client.post(PREFIX + "/register", json=user_create_payload)
+    first_user_create_response = mock_client.post(USERS_PREFIX + "/register", json=user_create_payload)
     
     first_user_create_status_code = first_user_create_response.status_code
     assert first_user_create_status_code == 201
 
     # get list of users after first user registration
-    first_get_user_list_response = mock_client.get(PREFIX + "/")
+    first_get_user_list_response = mock_client.get(USERS_PREFIX + "/")
     
     first_get_user_list_status_code = first_get_user_list_response.status_code
     assert first_get_user_list_status_code == 200
@@ -89,13 +90,13 @@ def test_user_list(mock_client, user_create_payload):
     new_user_create_payload["username"] = "new_mock_username"
     new_user_create_payload["email"] = "new_mock_email@email.com"
 
-    second_user_create_response = mock_client.post(PREFIX + "/register", json=new_user_create_payload)
+    second_user_create_response = mock_client.post(USERS_PREFIX + "/register", json=new_user_create_payload)
     
     second_user_create_status_code = second_user_create_response.status_code
     assert second_user_create_status_code == 201
 
     # get list of users after second user registration
-    second_get_user_list_response = mock_client.get(PREFIX + "/")
+    second_get_user_list_response = mock_client.get(USERS_PREFIX + "/")
     
     second_get_user_list_status_code = second_get_user_list_response.status_code
     assert second_get_user_list_status_code == 200
@@ -111,7 +112,7 @@ def test_user_list(mock_client, user_create_payload):
 
 def test_user_delete(mock_client, user_create_payload):
     # create a user
-    user_create_response = mock_client.post(PREFIX + "/register", json=user_create_payload)
+    user_create_response = mock_client.post(USERS_PREFIX + "/register", json=user_create_payload)
     
     user_create_status_code = user_create_response.status_code
     assert user_create_status_code == 201
@@ -119,13 +120,13 @@ def test_user_delete(mock_client, user_create_payload):
     user_id = user_create_response.json()["id"]
 
     # delete the user
-    user_delete_response = mock_client.delete(PREFIX + f"/{user_id}")
+    user_delete_response = mock_client.delete(USERS_PREFIX + f"/{user_id}")
 
     user_delete_status_code = user_delete_response.status_code
     assert user_delete_status_code == 200
 
     # check if user is still registered
-    get_user_list_response = mock_client.get(PREFIX + "/")
+    get_user_list_response = mock_client.get(USERS_PREFIX + "/")
     
     get_user_list_status_code = get_user_list_response.status_code
     assert get_user_list_status_code == 200
@@ -135,13 +136,13 @@ def test_user_delete(mock_client, user_create_payload):
 
 def test_get_current_user_info(mock_client, user_create_payload, user_login_payload):
     # create a user
-    user_create_response = mock_client.post(PREFIX + "/register", json=user_create_payload)
+    user_create_response = mock_client.post(USERS_PREFIX + "/register", json=user_create_payload)
     
     user_create_status_code = user_create_response.status_code
     assert user_create_status_code == 201
 
     # get a jwt token
-    get_token_response = mock_client.post("/auth/token", data=user_login_payload, headers={"content-type": "application/x-www-form-urlencoded"})
+    get_token_response = mock_client.post(AUTH_PREFIX + "/token", data=user_login_payload, headers={"content-type": "application/x-www-form-urlencoded"})
     
     get_token_status_code = get_token_response.status_code    
     assert get_token_status_code == 200
@@ -149,7 +150,7 @@ def test_get_current_user_info(mock_client, user_create_payload, user_login_payl
     access_token = get_token_response.json()["access_token"]
 
     # get current user info
-    current_user_info_response = mock_client.get(PREFIX + "/me", headers={"Authorization":f"Bearer {access_token}"})
+    current_user_info_response = mock_client.get(USERS_PREFIX + "/me", headers={"Authorization":f"Bearer {access_token}"})
     
     current_user_info_status_code = current_user_info_response.status_code
     assert current_user_info_status_code == 200
