451606d01f91593a3e4263048887ee0ad9986396
Samson <samsonho2005@gmail.com>
2025-11-16 19:28:43 +0800

    [fix](s3_bucket) fix proper async

--- app/s3/s3_bucket.py
+++ app/s3/s3_bucket.py
@@ -8,13 +8,11 @@ session = aiobotocore.session.get_session()
 BUCKET_NAME = 'dot-s3'
 
 async def get_s3_client():
-    s3_client = await session.create_client(
+    async with session.create_client(
         service_name='s3',
         aws_access_key_id=settings.FILE_STORAGE_ACCESS_KEY_ID,
         aws_secret_access_key=settings.FILE_STORAGE_SECRET_ACCESS_KEY,
         endpoint_url=settings.FILE_STORAGE_URL,
         region_name=settings.FILE_STORAGE_REGION
-    )
-    
-    async with s3_client as s3:        
-        return s3
+    ) as s3_client:
+        return s3_client
