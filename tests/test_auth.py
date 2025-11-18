9f4252acf5efc13fc56807818c19cc53fb216e98
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-11-19 01:10:37 +0800

    [refactor](auth) refactor test_auth.py using helper functions

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,11 +1,15 @@
 # tests/test_users.py
+import utils
+
 USERS_PREFIX = "/users"
 AUTH_PREFIX = "/auth"
 
 def test_invalid_token_authentication(mock_client):
     # attempt to authenticate with invalid token
     invalid_access_token = "invalid_access_token"
-    current_user_info_response = mock_client.get(USERS_PREFIX + "/me", headers={"Authorization":f"Bearer {invalid_access_token}"})
+    authorization_headers = utils.get_authorization_headers(invalid_access_token)
+
+    current_user_info_response = mock_client.get(USERS_PREFIX + "/me", headers=authorization_headers)
     
     current_user_info_status_code = current_user_info_response.status_code
     assert current_user_info_status_code == 401
@@ -13,14 +17,13 @@ def test_invalid_token_authentication(mock_client):
     current_user_info_json = current_user_info_response.json()
     assert current_user_info_json["detail"] == "Could not validate credentials"
 
-def test_valid_login(mock_client, user_create_payload, user_login_payload):
+def test_valid_login(mock_client, user_create_payload):
     # create a user
-    user_create_response = mock_client.post(USERS_PREFIX + "/register", json=user_create_payload)
-    
-    user_create_status_code = user_create_response.status_code
-    assert user_create_status_code == 201
+    utils.create_new_user(mock_client, user_create_payload)
 
     # login for jwt token
+    user_login_payload = utils.get_user_login_payload(user_create_payload)
+
     get_token_response = mock_client.post(AUTH_PREFIX + "/token", data=user_login_payload, headers={"content-type": "application/x-www-form-urlencoded"})
     
     get_token_status_code = get_token_response.status_code    
@@ -32,16 +35,19 @@ def test_valid_login(mock_client, user_create_payload, user_login_payload):
     access_token = get_token_response.json()["access_token"]
 
     # check if token is valid
-    current_user_info_response = mock_client.get(USERS_PREFIX + "/me", headers={"Authorization":f"Bearer {access_token}"})
+    authorization_headers = utils.get_authorization_headers(access_token)
+    current_user_info_response = mock_client.get(USERS_PREFIX + "/me", headers=authorization_headers)
     
     current_user_info_status_code = current_user_info_response.status_code
     assert current_user_info_status_code == 200
     
-    current_user_info_response_json = current_user_info_response.json()
-    assert current_user_info_response_json["email"] == user_create_payload["email"]
+    current_user_info_json = current_user_info_response.json()
+    assert current_user_info_json["email"] == user_create_payload["email"]
 
-def test_invalid_username_login(mock_client, user_login_payload):
+def test_invalid_username_login(mock_client, user_create_payload):
     # login for jwt token with invalid username
+    user_login_payload = utils.get_user_login_payload(user_create_payload)
+
     invaid_username_login_payload = user_login_payload.copy()
     invaid_username_login_payload["username"] = "invalid_mock_username"
 
@@ -53,8 +59,10 @@ def test_invalid_username_login(mock_client, user_login_payload):
     get_token_json = get_token_response.json()
     assert get_token_json["detail"] == "Incorrect email or password"
 
-def test_invalid_password_login(mock_client, user_login_payload):
+def test_invalid_password_login(mock_client, user_create_payload):
     # login for jwt token with invalid password
+    user_login_payload = utils.get_user_login_payload(user_create_payload)
+
     invaid_password_login_payload = user_login_payload.copy()
     invaid_password_login_payload["password"] = "invalid_mock_password"
 
