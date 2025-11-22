faa778ea979a4ef14a89bde4cb0b5359a9d6b737
SamayUniversity <193707933+SamayUniversity@users.noreply.github.com>
2025-11-22 20:19:13 +0800

    [fix](license) use sqlalchemy provided boolean values for LicenseKey table model

--- app/models/license_keys.py
+++ app/models/license_keys.py
@@ -1,11 +1,12 @@
 from sqlalchemy import (Column, Integer, String, Boolean,DateTime, ForeignKey, func)
+from sqlalchemy import sql
 from app.db.database import Base
 
 class LicenseKey(Base):
     __tablename__ = "license_keys"
     id = Column(Integer, primary_key=True, index=True)
     key_string = Column(String(255), unique=True, nullable=False, index=True)
-    is_used = Column(Boolean, nullable=False, server_default="false")
+    is_used = Column(Boolean, nullable=False, server_default=sql.false())
 
     activated_by_user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
     
